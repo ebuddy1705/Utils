95a0b02f804072157f87181f254b56bbaa06926a
ninhld <luongduyninh@gmail.com>
2014-10-21 14:35:04 +0700

    uodate virtual.c and dynamic_cast.c

--- cplusplus/dynamic_cast.cpp
+++ cplusplus/dynamic_cast.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>      /* printf, scanf, puts, NULL */
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
+
+
 using namespace std;
 
 class Base
@@ -37,41 +39,265 @@ Base* CreateRandom()
 
 //====================main==============================
 
-#if 0
-int main()
-{
-    for( int n = 0; n < 10; ++n )
-    {
-        Base* base = CreateRandom();
+#define NOMALY 				1
+#define DYNAMIC_CAST 		0
+#define STATIC_CAST 		0
+#define REINTERPRET_CAST	0
 
-            base->DoIt();
+#if NOMALY
+	int main()
+	{
+		
+		for( int n = 0; n < 10; ++n )
+		{
+			
+			Base* base = CreateRandom();
 
-        Bar* bar = (Bar*)base;
-        bar->BarIt();
-    }
-  return 0;
-}
+				base->DoIt();
 
-#else
-int main()
-{
+			Bar* bar = (Bar*)base;
+			bar->BarIt();
+			
+			delete base; base = NULL;  
+			printf("--------------NOMALY------------- \n");
+		}
+	  return 0;
+	}
+
+#elif DYNAMIC_CAST
+
+	int main()
+	{
 
 
 
-    for( int n = 0; n < 10; ++n )
-    {
-        Base* base = CreateRandom();
+		for( int n = 0; n < 10; ++n )
+		{
+			Base* base = CreateRandom();
 
-        base->DoIt();
+			base->DoIt();
 
-        Bar* bar = dynamic_cast<Bar*>(base);
-        Foo* foo = dynamic_cast<Foo*>(base);
-        if( bar )
-            bar->BarIt();
-        if( foo )
-            foo->FooIt();
-    }
-  return 0;
+			Bar* bar = dynamic_cast<Bar*>(base);
+			Foo* foo = dynamic_cast<Foo*>(base);
+			if( bar )
+				bar->BarIt();
+			if( foo )
+				foo->FooIt();
+			delete base; base = NULL;    
+			printf("-------------DYNAMIC_CAST-------------- \n");
+		}
+	  return 0;
 
-}
+	}
+#elif STATIC_CAST
+
+	int main()
+	{
+
+
+
+		for( int n = 0; n < 10; ++n )
+		{
+			Base* base = CreateRandom();
+
+			base->DoIt();
+
+			Bar* bar = static_cast<Bar*>(base);
+			Foo* foo = static_cast<Foo*>(base);
+			if( bar )
+				bar->BarIt();
+			if( foo )
+				foo->FooIt();
+			delete base; base = NULL;    
+			printf("-------------STATIC_CAST-------------- \n");
+		}
+	  return 0;
+
+	}
+	
+#elif REINTERPRET_CAST
+
+	int main()
+	{
+
+
+
+		for( int n = 0; n < 10; ++n )
+		{
+			Base* base = CreateRandom();
+
+			base->DoIt();
+
+			Bar* bar = reinterpret_cast<Bar*>(base);
+			Foo* foo = reinterpret_cast<Foo*>(base);
+			if( bar )
+				bar->BarIt();
+			if( foo )
+				foo->FooIt();
+			delete base; base = NULL;    
+			printf("-------------REINTERPRET_CAST-------------- \n");
+		}
+	  return 0;
+
+	}	
+	
 #endif
+
+
+
+
+/*
+ 
+ Bar 
+baring It... 
+--------------NOMALY------------- 
+Foo 
+baring It... 
+--------------NOMALY------------- 
+Bar 
+baring It... 
+--------------NOMALY------------- 
+Bar 
+baring It... 
+--------------NOMALY------------- 
+Bar 
+baring It... 
+--------------NOMALY------------- 
+Bar 
+baring It... 
+--------------NOMALY------------- 
+Foo 
+baring It... 
+--------------NOMALY------------- 
+Foo 
+baring It... 
+--------------NOMALY------------- 
+Bar 
+baring It... 
+--------------NOMALY------------- 
+Bar 
+baring It... 
+--------------NOMALY------------- 
+
+
+
+Bar 
+baring It... 
+-------------DYNAMIC_CAST-------------- 
+Foo 
+Fooing It... 
+-------------DYNAMIC_CAST-------------- 
+Bar 
+baring It... 
+-------------DYNAMIC_CAST-------------- 
+Bar 
+baring It... 
+-------------DYNAMIC_CAST-------------- 
+Bar 
+baring It... 
+-------------DYNAMIC_CAST-------------- 
+Bar 
+baring It... 
+-------------DYNAMIC_CAST-------------- 
+Foo 
+Fooing It... 
+-------------DYNAMIC_CAST-------------- 
+Foo 
+Fooing It... 
+-------------DYNAMIC_CAST-------------- 
+Bar 
+baring It... 
+-------------DYNAMIC_CAST-------------- 
+Bar 
+baring It... 
+-------------DYNAMIC_CAST--------------
+
+
+
+
+Bar 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Foo 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Foo 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Foo 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------STATIC_CAST--------------
+
+
+
+
+Bar 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Foo 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Foo 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Foo 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+Bar 
+baring It... 
+Fooing It... 
+-------------REINTERPRET_CAST-------------- 
+
+*/
--- cplusplus/virtual.cpp
+++ cplusplus/virtual.cpp
@@ -1,91 +1,100 @@
 
-#ifdef test1
-#include<iostream>
+#if 1
 
-using namespace std;
+	#include<iostream>
 
-class Base
+	using namespace std;
 
-{
+	class Base
 
-int x;
+	{
 
-public:
+	int x;
 
-    virtual void setvalue(int i){x=i;cout<<"Lop co so \n";}
+	public:
 
-    virtual int getvalue(){return x;}
+		virtual void setvalue(int i){x=i;cout<<"Lop co so \n";}
 
-};
+		virtual int getvalue(){return x;}
 
-class Derived:public Base
+	};
 
-{
+	class Derived:public Base
 
-    int y;
+	{
 
-public:
+		int y;
 
-    void setvalue(int i){y=i;cout<<"Lop dan xuat \n";}
-    int getvalue(){return y;}
+	public:
 
-};
+		void setvalue(int i){y=i;cout<<"Lop dan xuat \n";}
+		int getvalue(){return y;}
 
-int main()
+	};
 
-{
+	int main()
 
-    Base*p, o1;
+	{
 
-    Derived obj, *p1;
+		Base*p, o1;
 
-    p=&obj; // con trỏ lớp cơ sở trỏ đến đối tượng của lớp dẫn xuất
+		Derived obj, *p1;
 
-    p->setvalue(5); //nhận giá trị của lớp dẫn xuất
+		p=&obj; // con trỏ lớp cơ sở trỏ đến đối tượng của lớp dẫn xuất			
 
-    p=&o1; //trỏ đến con trỏ trong cùng lớp cơ sở
+		p->setvalue(5); //nhận giá trị của lớp dẫn xuất
 
-    p->setvalue(5); //nhận giá trị của lớp cơ sở
+		p=&o1; //trỏ đến con trỏ trong cùng lớp cơ sở
 
-    return 0;
+		p->setvalue(5); //nhận giá trị của lớp cơ sở
 
-}
+		return 0;
+
+	}
 
 #endif
 
 
+#ifdef test2
 
-#include<iostream>
-using namespace std;
-class Base
-{
-    int x;
-public:
-    virtual void setvalue(int i)=0;
-    virtual int getvalue()=0;
-};
-class Derived1:public Base{
-    int y;
-public:
-    void setvalue(int i){y=i;cout<<"Lop dan xuat 1 \n";}
-    int getvalue(){return y;}
-};
-class Derived2:public Derived1{
-    int z;
+	#include<iostream>
+	using namespace std;
 
-public:
-    void setvalue(int i){z=i;cout<<"Lop dan xuat 2 \n";}
-    int getvalue(){return z;}
-};
+	class Base
+	{
+		int x;
+	public:
+		virtual void setvalue(int i)=0;
+		virtual int getvalue()=0;
+	};
 
-int main(){
-    Base*p;
-//    Base o1; //sai vì lớp cơ sở ảo không có đối tượng riêng của nó
-    Derived1 obj;
-    Derived2 o2;
-    p=&obj; //trỏ đến đối tượng của lớp dẫn xuất thứ 1
-    p->setvalue(5);
-    p=&o2; //trỏ đến đối tượng của lớp dẫn xuất thứ 2
-    p->setvalue(6);//kết quả sẽ là: “lớp dẫn xuất 2″ nếu ta không định nghĩa lại ở Derived1
-    return 0;
-}
+	class Derived1:public Base{
+		int y;
+	public:
+		void setvalue(int i){y=i;cout<<"Lop dan xuat 1 \n";}
+		int getvalue(){return y;}
+	};
+
+	class Derived2:public Derived1{
+		int z;
+
+	public:
+		void setvalue(int i){z=i;cout<<"Lop dan xuat 2 \n";}
+		int getvalue(){return z;}
+	};
+
+	int main(){
+		Base*p;
+	//    Base o1; //sai vì lớp cơ sở ảo không có đối tượng riêng của nó
+
+		Derived1 obj;
+		Derived2 o2;
+		
+		p=&obj; //trỏ đến đối tượng của lớp dẫn xuất thứ 1
+		p->setvalue(5);
+		p=&o2; //trỏ đến đối tượng của lớp dẫn xuất thứ 2
+		p->setvalue(6);//kết quả sẽ là: “lớp dẫn xuất 2″ nếu ta không định nghĩa lại ở Derived1
+		return 0;
+	}
+
+#endif
